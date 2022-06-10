882984560ff02a624e5758b4e95a081de10da040
praset <prasetyaarfanto@gmail.com>
2022-06-10 12:56:35 +0700

    add on click function for button in home

--- app/src/main/java/com/project/banksampah/view/main/MainActivity.kt
+++ app/src/main/java/com/project/banksampah/view/main/MainActivity.kt
@@ -1,12 +1,31 @@
 package com.project.banksampah.view.main
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.project.banksampah.R
+import com.project.banksampah.databinding.ActivityMainBinding
+import com.project.banksampah.databinding.ContentMainBinding
+import com.project.banksampah.view.history.HistoryActivity
+import com.project.banksampah.view.jenissampah.SampahTypeActivity
+import com.project.banksampah.view.pickup.PickUpActivity
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var contentMainBinding: ContentMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        contentMainBinding = ContentMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+        contentMainBinding.cvPickup.setOnClickListener {
+            startActivity(Intent(this, PickUpActivity::class.java))
+        }
+        contentMainBinding.cvHistory.setOnClickListener {
+            startActivity(Intent(this, HistoryActivity::class.java))
+        }
+        contentMainBinding.cvKategori.setOnClickListener {
+            startActivity(Intent(this, SampahTypeActivity::class.java))
+        }
     }
 }
