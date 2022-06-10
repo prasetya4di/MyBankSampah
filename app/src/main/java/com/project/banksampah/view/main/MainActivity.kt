4710ccfa87765ff18694c6f6dd4f7d2d2152baf9
praset <prasetyaarfanto@gmail.com>
2022-06-10 16:12:00 +0700

    update main activity on click

--- app/src/main/java/com/project/banksampah/view/main/MainActivity.kt
+++ app/src/main/java/com/project/banksampah/view/main/MainActivity.kt
@@ -4,7 +4,6 @@ import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.project.banksampah.databinding.ActivityMainBinding
-import com.project.banksampah.databinding.ContentMainBinding
 import com.project.banksampah.view.history.HistoryActivity
 import com.project.banksampah.view.jenissampah.SampahTypeActivity
 import com.project.banksampah.view.pickup.PickUpActivity
@@ -13,20 +12,18 @@ import dagger.hilt.android.AndroidEntryPoint
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private lateinit var contentMainBinding: ContentMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
-        contentMainBinding = ContentMainBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
-        contentMainBinding.cvPickup.setOnClickListener {
+        binding.content.cvPickup.setOnClickListener {
             startActivity(Intent(this, PickUpActivity::class.java))
         }
-        contentMainBinding.cvHistory.setOnClickListener {
+        binding.content.cvHistory.setOnClickListener {
             startActivity(Intent(this, HistoryActivity::class.java))
         }
-        contentMainBinding.cvKategori.setOnClickListener {
+        binding.content.cvKategori.setOnClickListener {
             startActivity(Intent(this, SampahTypeActivity::class.java))
         }
     }
