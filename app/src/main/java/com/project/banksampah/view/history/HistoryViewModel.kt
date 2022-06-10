c4c2f4138ac7ef8170918f74d3e6cac28cfba69a
praset <prasetyaarfanto@gmail.com>
2022-06-10 14:21:48 +0700

    add history view model

---
+++ app/src/main/java/com/project/banksampah/view/history/HistoryViewModel.kt
@@ -0,0 +1,30 @@
+package com.project.banksampah.view.history
+
+import androidx.lifecycle.LiveData
+import com.project.banksampah.model.entity.PickUp
+import com.project.banksampah.usecase.DeletePickUp
+import com.project.banksampah.usecase.GetAllPickUp
+import com.project.banksampah.usecase.GetTotalBalance
+import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.core.Completable
+import io.reactivex.rxjava3.schedulers.Schedulers
+import javax.inject.Inject
+
+@HiltViewModel
+class HistoryViewModel @Inject constructor(
+    getAllPickUp: GetAllPickUp,
+    getTotalBalance: GetTotalBalance,
+    private val deletePickUp: DeletePickUp
+) {
+    var totalSaldo: LiveData<Int> = getTotalBalance()
+    var pickUps: LiveData<List<PickUp>> = getAllPickUp()
+
+    fun deletePickUp(pickUp: PickUp) {
+        Completable.fromAction {
+            deletePickUp(pickUp)
+        }
+            .subscribeOn(Schedulers.io())
+            .observeOn(Schedulers.single())
+            .subscribe()
+    }
+}
