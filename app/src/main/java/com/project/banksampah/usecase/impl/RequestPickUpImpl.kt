6ba5253033ae3a144cef2486d3be2710a7597b33
praset <prasetyaarfanto@gmail.com>
2022-06-10 13:27:33 +0700

    add request pick up use case

---
+++ app/src/main/java/com/project/banksampah/usecase/impl/RequestPickUpImpl.kt
@@ -0,0 +1,13 @@
+package com.project.banksampah.usecase.impl
+
+import com.project.banksampah.model.entity.PickUp
+import com.project.banksampah.repository.PickUpRepository
+import com.project.banksampah.usecase.RequestPickUp
+import javax.inject.Inject
+
+class RequestPickUpImpl @Inject constructor(private val pickUpRepository: PickUpRepository) :
+    RequestPickUp {
+    override fun invoke(pickUp: PickUp) {
+        pickUpRepository.insert(pickUp)
+    }
+}
