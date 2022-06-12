cff72c684da98d7c2006ed8ec3e7b9636ec4dda4
praset <prasetyaarfanto@gmail.com>
2022-06-12 23:11:13 +0700

    add function to save total price in pickup view model

--- app/src/main/java/com/project/banksampah/view/pickup/PickUpViewModel.kt
+++ app/src/main/java/com/project/banksampah/view/pickup/PickUpViewModel.kt
@@ -1,5 +1,7 @@
 package com.project.banksampah.view.pickup
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.project.banksampah.model.entity.PickUp
 import com.project.banksampah.usecase.RequestPickUp
@@ -12,6 +14,37 @@ import javax.inject.Inject
 class PickUpViewModel @Inject constructor(
     private val requestPickUp: RequestPickUp
 ) : ViewModel() {
+    private var _weight: MutableLiveData<Int> = MutableLiveData()
+    private var _totalPrice: MutableLiveData<Int> = MutableLiveData()
+    private var _selectedCategory: MutableLiveData<Int> = MutableLiveData()
+    private var _pricePerKg: MutableLiveData<Int> = MutableLiveData()
+
+    init {
+        _totalPrice.value = 0
+        _selectedCategory.value = 0
+        _pricePerKg.value = 0
+    }
+
+    val totalPrice: LiveData<Int> = _totalPrice
+    val selectedCategory: LiveData<Int> = _selectedCategory
+    val pricePerKg: LiveData<Int> = _pricePerKg
+    val weight: LiveData<Int> = _weight
+
+    fun setSelectedCategory(selectedCategory: Int, pricePerKg: Int) {
+        _selectedCategory.postValue(selectedCategory)
+        _pricePerKg.postValue(pricePerKg)
+        if (weight.value != null) {
+            _totalPrice.postValue(weight.value!!.times(pricePerKg))
+        }
+    }
+
+    fun changeKg(kg: Int) {
+        _weight.postValue(kg)
+        if (pricePerKg.value != null) {
+            _totalPrice.postValue(kg.times(pricePerKg.value!!))
+        }
+    }
+
     fun addPickUp(
         name: String,
         type: String,
