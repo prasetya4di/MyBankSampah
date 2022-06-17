5c312a9cf02a4ae8f0a14aa17327fe787bccf399
praset <prasetyaarfanto@gmail.com>
2022-06-17 22:59:00 +0700

    add go to register function

--- app/src/main/java/com/project/banksampah/view/login/LoginActivity.kt
+++ app/src/main/java/com/project/banksampah/view/login/LoginActivity.kt
@@ -1,8 +1,10 @@
 package com.project.banksampah.view.login
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.project.banksampah.databinding.ActivityLoginBinding
+import com.project.banksampah.view.register.RegisterActivity
 
 class LoginActivity : AppCompatActivity() {
     private lateinit var binding: ActivityLoginBinding
@@ -11,5 +13,9 @@ class LoginActivity : AppCompatActivity() {
         binding = ActivityLoginBinding.inflate(layoutInflater)
         val view = binding.root
         setContentView(view)
+
+        binding.btnRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
     }
 }
